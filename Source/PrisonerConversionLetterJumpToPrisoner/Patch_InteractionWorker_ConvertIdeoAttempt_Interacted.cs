8227ae354c00419ffa6adffdb2a8a8d68d04a2b4
1trickPonyta <1trickPonyta@kangaroostandard.com>
2022-03-11 07:25:00 -0800

    Add description, add prisoner conversion jump to location target fix

---
+++ Source/PrisonerConversionLetterJumpToPrisoner/Patch_InteractionWorker_ConvertIdeoAttempt_Interacted.cs
@@ -0,0 +1,24 @@
+﻿using HarmonyLib;
+using RimWorld;
+using RimWorld.Planet;
+using System.Linq;
+using Verse;
+
+namespace IdeologyPatch.PrisonerConversionLetterJumpToPrisoner
+{
+    public static class Patch_InteractionWorker_ConvertIdeoAttempt_Interacted
+    {
+        [HarmonyPatch(typeof(InteractionWorker_ConvertIdeoAttempt))]
+        [HarmonyPatch(nameof(InteractionWorker_ConvertIdeoAttempt.Interacted))]
+        public static class Patch_PreceptComp_SelfTookMemoryThought_get_TraitsAffecting
+        {
+            public static void Postfix(Pawn initiator, Pawn recipient, LookTargets lookTargets)
+            {
+                if (lookTargets != null)
+                {
+                    lookTargets.targets = (new GlobalTargetInfo[] { recipient, initiator }).ToList();
+                }
+            }
+        }
+    }
+}
