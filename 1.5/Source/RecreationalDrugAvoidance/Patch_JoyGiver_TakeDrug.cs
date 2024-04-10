fb1b66398e75d230701343fc9b19cf6f950d318f
1trickPwnyta <sarahpace37@gmail.com>
2024-04-09 22:54:37 -0700

    Stopped pawns against recreational drug use from using recreational drugs

---
+++ 1.5/Source/RecreationalDrugAvoidance/Patch_JoyGiver_TakeDrug.cs
@@ -0,0 +1,19 @@
+﻿using HarmonyLib;
+using RimWorld;
+using Verse;
+
+namespace IdeologyPatch.ProstheticsCount
+{
+    [HarmonyPatch(typeof(JoyGiver_TakeDrug))]
+    [HarmonyPatch(nameof(JoyGiver_TakeDrug.GetChance))]
+    public static class Patch_JoyGiver_TakeDrug_GetChance
+    {
+        public static void Postfix(Pawn pawn, ref float __result)
+        {
+            if (pawn.IsTeetotaler())
+            {
+                __result = 0f;
+            }
+        }
+    }
+}
